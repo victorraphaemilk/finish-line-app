8ef40279eff09d7ac82ccecc85ef7b525bf4cea7
Victor <sivictorraphaelleitemaciel@gmail.com>
2026-02-28 17:18:09 -0300

    feat: adiciona routerlinks e material icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: Login},
+    {path: 'dashboard', component: Dashboard},
+    {path: '**', redirectTo: 'login'}
+
+];
