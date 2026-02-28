c470eec979eb43d2b4cc039e4de02deb944fc320
Victor <sivictorraphaelleitemaciel@gmail.com>
2026-02-28 15:20:37 -0300

    fix: remove ssr

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, finish-line-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, finishline');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('finish-line-app');
+  protected readonly title = signal('finishline');
 }
