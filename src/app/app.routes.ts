fb490da3229220da27856692821b2639dea6edfc
Victor <sivictorraphaelleitemaciel@gmail.com>
2026-02-28 21:12:34 -0300

    teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
+    {path: 'login', component: Login, pathMatch:'full'},
     {path: 'login', component: Login},
     {path: 'dashboard', component: Dashboard},
     {path: '**', redirectTo: 'login'}
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import {FormGroup, ReactiveFormsModule, FormBuilder, Form, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+
+  loginForm! : FormGroup;
+  constructor(private fb:FormBuilder){
+
+    this.loginForm = this.fb.group({
+      
+      "email": ['', [Validators.required, Validators.email]],
+      "senha": ['', Validators.required]
+
+    });
+  }
+
+  fazerLogin(){
+    console.log(this.loginForm.value)
+  }
+}
